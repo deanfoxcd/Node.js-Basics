35bf11052028f2d3bf06903d565f32bf4bdf8372
Dean Fox <deanfoxcd@gmail.com>
2025-03-13 07:36:35 -0400

    add basic routes setup

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ import express from 'express';
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware');
-  next();
+app.use('/add-product', (req, res, next) => {
+  console.log('In another middleware');
+  res.send('<h1>Add product</h1>');
 });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
   console.log('In another middleware');
   res.send('<h1>Hello from express</h1>');
 });
