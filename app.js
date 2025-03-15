8b82b96e2edaf46af371a8f251a67c7dec7d3987
Dean Fox <deanfoxcd@gmail.com>
2025-03-14 21:27:15 -0400

    add input field for adding a product and added  body parser

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 // import http from 'http';
 import express from 'express';
+import bodyParser from 'body-parser';
 
 // import { requestHandler } from './routes-v1.js';
 
 const app = express();
 
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded());
+
 app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware');
-  res.send('<h1>Add product</h1>');
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'/><button type='submit'>Add product</button></form>"
+  );
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-  console.log('In another middleware');
+  console.log('Home');
   res.send('<h1>Hello from express</h1>');
 });
 
